49eca02c26ea981133181a044dee06d31e3ec6d8
Ramasarma <ramasarma.pakala@gmail.com>
2022-12-10 11:30:48 +0530

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
-class hello {
-public Static void main(String[] args){
-System.out.println("Hello Raam");
+class Hello{
+public static void main(String [] args){
+
+System.out.println("Hello world");
 }
 }
